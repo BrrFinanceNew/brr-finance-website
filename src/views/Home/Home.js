a4f9d5a3f5119e013e99b36b1c5323272f7bc096
dNNar0L <sslorand1993@gmail.com>
2022-03-06 22:21:38 +0200

    asd

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -160,7 +160,7 @@ const Home = () => {
           <Paper style={{ backgroundColor: "transparent", boxShadow: "none", border: "1px solid var(--white)" }}>
             <Box p={4}>
               <h2>Welcome to Brr Finance!</h2>
-              <p>An algorithmic stablecoin on the Fantom Opera blockchain, pegged to the price of 1 BUSD</p>
+              <p>An algorithmic stablecoin on the Binance Smart Chain, pegged to the price of 1 BUSD</p>
               <p>Cash utilizes multiple bonding mechanisms at the <StyledLink href="/rebates">DAO</StyledLink> as well as seigniorage.</p>
               <p>
                 Stake your CASH-BUSD LP in the <StyledLink href="/farms">Farm</StyledLink> to earn PRINTER rewards.
